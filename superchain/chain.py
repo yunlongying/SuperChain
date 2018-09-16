d4c02ccee685ecb9848d1790dd62660633ed911b
yunlongying <fengyuan1210@163.com>
2018-09-16 12:06:03 +0800

    Add chain code and unit tests in SuperChain

---
+++ superchain/chain.py
@@ -0,0 +1,27 @@
+
+
+class chain():
+    def __init__(self, *args):
+        if not args:
+            self.keys = None
+        elif isinstance(args[0], str):
+            self.keys = list(args)
+        elif isinstance(args[0], (tuple, list)):
+            self.keys = list(args[0])
+        else:
+            raise Exception('args must be a list or tuple of types')
+
+    def getkeys(self):
+        return self.keys
+
+
+if __name__ == '__main__':
+    c0 = chain()
+    c1 = chain('app', 'job')
+    c2 = chain(['job', 'model'])
+    c3 = chain(('job', 'model'))
+    c3 = chain({'job', 'model'})
+    print(c0.getkeys())
+    print(c1.getkeys())
+    print(c2.getkeys())
+    print(c3.getkeys())
