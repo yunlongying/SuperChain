79d65d0c99921c7dd6aa5f5bec5a33db6a76bd6c
yunlongying <fengyuan1210@163.com>
2018-09-16 22:11:32 +0800

    Add unit tests when create chain

--- superchain/chain.py
+++ superchain/chain.py
@@ -14,6 +14,16 @@ class chain():
     def getkeys(self):
         return self.keys
 
+    # def add(self, *args):
+    #     if not args:
+    #         raise Exception('inputs is illegal')
+    #     elif isinstance(args[0], str):
+    #         self.keys = list(args)
+    #     elif isinstance(args[0], (tuple, list)):
+    #         self.keys = list(args[0])
+    #     else:
+    #         raise Exception('args must be a list or tuple of types')
+
 
 if __name__ == '__main__':
     c0 = chain()
--- superchain/tests/tests.py
+++ superchain/tests/tests.py
@@ -19,6 +19,21 @@ class ChainTest(unittest.TestCase):
         chain_one = chain('app', 'jobs', 'model')
         self.assertEqual(self.keys, chain_one.getkeys())
 
+    def test_create_chain_when_keys_is_list(self):
+        chain_one = chain(['app', 'jobs', 'model'])
+        self.assertEqual(self.keys, chain_one.getkeys())
+
+    def test_create_chain_when_keys_is_tuple(self):
+        chain_one = chain(('app', 'jobs', 'model'))
+        self.assertEqual(self.keys, chain_one.getkeys())
+
+    # def test_create_chain_when_keys_is_illegal(self):
+    #     try:
+    #         chain_one = chain({'app', 'jobs', 'model'})
+    #     except Exception as exc:
+    #         print(str(exc))
+    #         self.assertEqual('args must be a list or tuple of types', chain_one.getkeys())
+
 
 if __name__ == '__main__':
     unittest.main()
