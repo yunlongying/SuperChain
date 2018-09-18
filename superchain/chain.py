78bee3d0da7ede2ced99526fdf9bf6953907c70c
yunlongying <fengyuan1210@163.com>
2018-09-18 22:08:28 +0800

    Add add/get/getdict function in superchain

--- superchain/chain.py
+++ superchain/chain.py
@@ -10,19 +10,43 @@ class chain():
             self.keys = list(args[0])
         else:
             raise Exception('args must be a list or tuple of types')
+        self.values = []
 
     def getkeys(self):
         return self.keys
 
-    # def add(self, *args):
-    #     if not args:
-    #         raise Exception('inputs is illegal')
-    #     elif isinstance(args[0], str):
-    #         self.keys = list(args)
-    #     elif isinstance(args[0], (tuple, list)):
-    #         self.keys = list(args[0])
-    #     else:
-    #         raise Exception('args must be a list or tuple of types')
+    def getvalues(self):
+        return self.values
+
+    def add(self, *args):
+        if not args:
+            raise Exception('inputs is illegal')
+        elif len(args) != 1:
+            self.values = list(args)
+        elif isinstance(args[0], (tuple, list)):
+            if len(args[0]) > len(self.keys):
+                raise Exception('The len of values(%s) > the len of keys(%s)' % (len(args[0]), len(self.keys)))
+            self.values = list(args[0])
+        else:
+            raise Exception('args must be a list or tuple of types')
+
+    def get(self, *args):
+        if not args:
+            return self.values
+        is_in = True
+        for arg in args:
+            if arg not in self.values:
+                is_in = False
+        if is_in:
+            return self.values
+        else:
+            return []
+
+    def getdict(self):
+        temp_dict = {}
+        for key in self.keys:
+            temp_dict[key] = self.values[self.keys.index(key)]
+        return temp_dict
 
 
 if __name__ == '__main__':
@@ -30,8 +54,21 @@ if __name__ == '__main__':
     c1 = chain('app', 'job')
     c2 = chain(['job', 'model'])
     c3 = chain(('job', 'model'))
-    c3 = chain({'job', 'model'})
+    # c3 = chain({'job', 'model'})
     print(c0.getkeys())
     print(c1.getkeys())
     print(c2.getkeys())
-    print(c3.getkeys())
+    print c2.getvalues()
+    # print(c3.getkeys())
+    c1.add(1,2)
+    print c1
+    print c1.values
+    print c1.get()
+    print c1.get(3)
+    print c2.getkeys()
+    c2.add(('a', 'b'))
+    print c2.getkeys()
+    print c2.getvalues()
+    print c2.get()
+    print c2.getdict()
+    print c2.getvalues()
